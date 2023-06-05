0fb34627d9c0a820e77ef74856317173396b5255
MayuriBadgujar <120549440+MayuriBadgujar@users.noreply.github.com>
2023-06-05 12:10:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+function toFahrenheit(celsius) {
+  var fahrenheit = (celsius * 9/5) + 32;
+  return fahrenheit;
+}
+var fahrenheit = toFahrenheit(0); // 32.00
+var fahrenheit = toFahrenheit(25); // 77.00
+var fahrenheit = toFahrenheit(-10);
